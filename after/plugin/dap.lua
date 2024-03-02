51b075bb3803d60ea15168a0cdd0a86b092ecf98
nikhil <contact@nikhilkowshik.com>
2024-03-02 02:53:10 -0800

    feat)nvim): add dap

---
+++ after/plugin/dap.lua
@@ -0,0 +1,137 @@
+local dap = require('dap');
+local masondap = require('mason-nvim-dap');
+
+local js_map_configs = {
+    ["pwa-node"] = {
+        configurations = {
+            {
+                type = "pwa-node",
+                request = "launch",
+                name = "Node: Launch file",
+                program = "${file}",
+                cwd = "${workspaceFolder}",
+                console = "integratedTerminal",
+            },
+            {
+                type = "pwa-node",
+                request = "attach",
+                name = "Node: Attach",
+                processId = require'dap.utils'.pick_process,
+                cwd = "${workspaceFolder}",
+            },
+            {
+                type = "pwa-node",
+                request = "launch",
+                name = "Node: Debug Mocha Test",
+                -- trace = true, -- include debugger info
+                program = "${file}",
+                runtimeExecutable = "node",
+                runtimeArgs = {
+                    "./node_modules/.bin/mocha",
+                    "-t",
+                    "0",
+                    "--exit"
+                },
+                rootPath = "${workspaceFolder}",
+                cwd = "${workspaceFolder}",
+                console = "integratedTerminal",
+            }
+        },
+        filetypes = { 'javascript', 'typescript' },
+    },
+    ["node-terminal"] = {
+        configurations = {
+            {
+                type = "node-terminal",
+                request = "launch",
+                name = "Start debugging terminal",
+                cwd = "${workspaceFolder}",
+            }
+        },
+        filetypes = { 'javascript', 'typescript' },
+    }
+}
+
+dap.defaults.fallback.switchbuf = 'useopen,usetab,newtab';
+dap.defaults.fallback.terminal_win_cmd = 'belowright new'
+
+masondap.setup({
+    ensure_installed = { 'js' };
+    handlers = {
+        function(config)
+            masondap.default_setup(config);
+        end;
+
+        js = function(config)
+            local base_config = vim.deepcopy(config);
+
+            local js_adapter = {
+                type = "server",
+                host = "localhost",
+                port = "${port}",
+                executable = {
+                    command = vim.fn.exepath('js-debug-adapter'),
+                    args = { "${port}" }
+                }
+            }
+
+            for name, adapter_setup in pairs(js_map_configs) do
+                local adapter_config = vim.deepcopy(base_config);
+
+                adapter_config.name = name
+                adapter_config.adapters = js_adapter;
+                adapter_config.filetypes = adapter_setup.filetypes;
+                adapter_config.configurations = adapter_setup.configurations;
+
+                masondap.default_setup(adapter_config);
+            end
+        end
+    }
+})
+
+vim.keymap.set('n', '<F9>', function() dap.continue() end)
+vim.keymap.set('n', '<F10>', function() dap.step_over() end)
+vim.keymap.set('n', '<F11>', function() dap.step_into() end)
+vim.keymap.set('n', '<F12>', function() dap.step_out() end)
+vim.keymap.set('n', '<Leader>db', function() dap.toggle_breakpoint() end)
+vim.keymap.set('n', '<Leader>dB', function() dap.set_breakpoint(vim.fn.input('Expression: ')) end)
+vim.keymap.set('n', '<Leader>dlp', function() dap.set_breakpoint(nil, nil, vim.fn.input('Log point message: ')) end)
+vim.keymap.set('n', '<Leader>dr', function() dap.repl.open() end)
+vim.keymap.set('n', '<Leader>dl', function() dap.run_last() end)
+
+vim.keymap.set('n', '<Leader>dcr', function()
+    require('dap.ext.vscode').load_launchjs(nil, { });
+    vim.notify "Reloaded vscode configs"
+end)
+
+vim.keymap.set({'n', 'v'}, '<Leader>dh', function()
+    require('dap.ui.widgets').hover()
+end)
+
+vim.keymap.set({'n', 'v'}, '<Leader>dp', function()
+    require('dap.ui.widgets').preview()
+end)
+
+vim.keymap.set('n', '<Leader>df', function()
+    local widgets = require('dap.ui.widgets')
+    widgets.centered_float(widgets.frames)
+end)
+
+vim.keymap.set('n', '<Leader>ds', function()
+    local widgets = require('dap.ui.widgets')
+
+    widgets.centered_float(widgets.scopes)
+end)
+
+vim.keymap.set('n', '<Leader>dut', function() 
+    require('dapui').toggle()
+end)
+
+vim.fn.execute('highlight Breakpoint guifg=#ff0000')
+vim.fn.execute('highlight RejectedBreakpoint guifg=#808080')
+
+vim.fn.sign_define('DapBreakpoint', {text='', texthl='Breakpoint', linehl='', numhl=''})
+vim.fn.sign_define('DapLogPoint', {text='󱂅', texthl='', linehl='', numhl=''})
+vim.fn.sign_define('DapStopped', {text='➡️', texthl='', linehl='', numhl=''})
+vim.fn.sign_define('DapBreakpointCondition', {text='', texthl='Breakpoint', linehl='', numhl=''})
+vim.fn.sign_define('DapBreakpointRejected', {text='', texthl='RejectedBreakpoint', linehl='', numhl=''})
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,4 +1,5 @@
 return {
+    -- lazy-load
     { "folke/lazy.nvim" };
 
     -- telescope:
@@ -9,18 +10,24 @@ return {
     { "nvim-treesitter/nvim-treesitter-context", lazy = false };
     { "nvim-treesitter/nvim-treesitter-textobjects", lazy = false };
 
+    -- Comment
     { "numToStr/Comment.nvim", lazy = false };
 
+    -- catppuccin theme
     { "catppuccin/nvim", name = "catppuccin", priority = 1000 };
 
+    -- lualine = status bar
     { "nvim-lualine/lualine.nvim", dependencies = { "nvim-tree/nvim-web-devicons" } };
 
+    -- nvim-tree - project explorer
     { "nvim-tree/nvim-tree.lua", dependencies = { "nvim-tree/nvim-web-devicons" } };
 
+    -- git
     { "lewis6991/gitsigns.nvim" };
 
     { "kkoomen/vim-doge", run = ":call doge#install()" };
 
+    -- cmp -- completion 
     {
         "hrsh7th/nvim-cmp";
         dependencies = {
@@ -34,12 +41,19 @@ return {
         };
     };
 
+    -- mason -- LSP
     { "williamboman/mason.nvim", lazy = false, config = function() require('mason').setup() end };
-    
+
     "williamboman/mason-lspconfig.nvim";
     "neovim/nvim-lspconfig";
-  
+
+    -- DAP
     { "mfussenegger/nvim-dap", lazy = false };
+
     "jay-babu/mason-nvim-dap.nvim";
+
     { "rcarriga/nvim-dap-ui", config = function() require('dapui').setup({}) end };
+
+    {'nvim-telescope/telescope-dap.nvim', dependencies = { 'mfussenegger/nvim-dap' } };
+
 }
